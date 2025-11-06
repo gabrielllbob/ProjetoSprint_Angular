0b7704ac40ed586538cbc84cbfbf29fdb93f5b76
GABRIEL LOPES LIMA <025.038500@universidadesenaicimatec.edu.br>
2025-11-06 10:35:33 -0300

    Componente login versao 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Login } from './login';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  alterarLembrar() {
+    this.login.lembrar = !this.login.lembrar;
+  }
+  formSubmit() {
+    console.log(this.login);
+  }
+  login: Login = {
+    login: '',
+    senha: '',
+    lembrar: false
+  }
 }
