4c79255adac01e1cc67252bb62d93f6669ec7ebe
GABRIEL LOPES LIMA <025.038500@universidadesenaicimatec.edu.br>
2025-11-13 11:20:22 -0300

    Completo

--- src/app/vehicle-dados/vehicle-dados.component.ts
+++ src/app/vehicle-dados/vehicle-dados.component.ts
@@ -1,41 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { VehicleService } from '../services/vehicle.service';
-import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { catchError, debounceTime, distinctUntilChanged, of, Subject, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-vehicle-dados',
   standalone: true,
-  imports: [NgIf, NgForOf, NgClass, FormsModule],
+  imports: [NgIf, NgClass, FormsModule],
   templateUrl: './vehicle-dados.component.html',
   styleUrls: ['./vehicle-dados.component.css']
 })
-export class VehicleDadosComponent {
+export class VehicleDadosComponent implements OnInit, OnDestroy {
   vin = '';
   vehicleData: any = null;
   errorMessage = '';
+  private searchVin$ = new Subject<string>();
+  private sub!: Subscription;
 
   constructor(private vehicleService: VehicleService) { }
 
-  onSerchVin() {
-    this.errorMessage = '';
-    this.vehicleData = null;
-
-    if (!this.vin.trim()) {
-      this.errorMessage = 'Digite um código VIN válido.';
-    }
-
-    this.vehicleService.getVehicleData(this.vin).subscribe({
-      next: (res) => {
+  ngOnInit() {
+    this.sub = this.searchVin$
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((vin) => {
+          if (!vin.trim()) {
+            this.vehicleData = null;
+            this.errorMessage = '';
+            return of(null);
+          }
+          return this.vehicleService.getVehicleData(vin).pipe(
+            catchError((err) => {
+              if (err.status === 400) {
+                this.showTemporaryError('Código VIN não encontrado.');
+              } else {
+                this.showTemporaryError('Erro ao buscar dados do veículo.');
+              }
+              return of(null);
+            })
+          );
+        })
+      )
+      .subscribe((res) => {
         this.vehicleData = res;
-      },
-      error: (err) => {
-        if (err.status === 400) {
-          this.errorMessage = 'Código VIN não encontrado.';
-        } else {
-          this.errorMessage = 'Erro ao buscar dados do veículo.';
-        }
-      }
-    });
+      });
+  }
+
+  onKeyUp() {
+    this.searchVin$.next(this.vin);
+  }
+
+  private showTemporaryError(message: string) {
+    this.errorMessage = message;
+    setTimeout(() => (this.errorMessage = ''), 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.sub) this.sub.unsubscribe();
   }
 }
